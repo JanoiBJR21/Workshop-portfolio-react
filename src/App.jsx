6e546250dc92c8373b9a4557ff99fe5968a7f3e7
Apichat Wongtala <apichat26870@gmail.com>
2024-05-07 17:09:17 +0700

    add: Component Services And Mywork to commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Services from './Components/Services/Services'
+import MyWork from './Components/MyWork/MyWork'
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
+      <MyWork/>
     </div>
   )
 }
